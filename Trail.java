e0872853a84b44d7139c1511a3409d90fb05c57e
gus9r9r <gus9r9r@gmail.com>
2016-05-24 13:40:06 -0400

    Add files via upload

---
+++ Trail.java
@@ -0,0 +1,27 @@
+package com.tutorial.main;
+
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Trail extends GameObject {
+
+	private float alpha = 1;
+	private Handler handler;
+	
+	public Trail(int x, int y, ID id) {
+		super(x, y, id);
+	}
+
+	public void tick() {
+		
+	}
+
+	public void render(Graphics g) {
+		
+	}
+
+	public Rectangle getBounds() {
+		return null;
+	}
+
+}
